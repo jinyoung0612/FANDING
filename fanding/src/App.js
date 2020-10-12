89512eb8ba6b2ae9728537a75461ed014134ae70
jell <jl1096@ajou.ac.kr>
2020-10-12 13:55:09 +0900

    업체 회원가입 폼 생성, DB 연동, NavBar수정

--- fanding/src/App.js
+++ fanding/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter,Route,Switch} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import SignUp from './components/auth/SignUp';
 import SignIn from './components/auth/SignIn';
+import SignUpCom from './components/auth/SingUpCom';
 
 class App extends Component{
     render(){
@@ -12,6 +13,7 @@ class App extends Component{
               <Navbar />
                 <Switch>
                   <Route path="/signup" component={SignUp} />
+                  <Route path="/signupcom" component={SignUpCom} />
                   <Route path="/signin" component={SignIn} />
                 </Switch>
               </div>
