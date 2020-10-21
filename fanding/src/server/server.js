52b8440877ca8cf9008b62b6904b0a88ade61059
Choi Jinyoung <bmdj123@gmail.com>
2020-10-21 20:24:03 +0900

    set express and server

---
+++ fanding/src/server/server.js
@@ -0,0 +1,16 @@
+const express =require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3001;
+app.use(cors({
+    origin:true,
+    credentials:true
+}));
+
+app.use(bodyParser.json());
+app.use('/',require('./routes/index'));
+
+app.listen(port, ()=>{
+    console.log(`express is running on ${port}`);
+})
