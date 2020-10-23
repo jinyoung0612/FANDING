b96cca826b5baf084e18d684a6889cc8409f60ee
JaeWook Jeong <br12345678@ajou.ac.kr>
2020-10-23 14:28:44 +0900

    이메일인증 구현

--- fanding/src/components/auth/SignUpCom.js
+++ fanding/src/components/auth/SignUpCom.js
@@ -1,96 +1,123 @@
-import React,{Component} from 'react';
+import React, { Component } from "react";
 // import { Redirect } from 'react-router-dom'
-import { connect } from 'react-redux';
-import { signUpCom } from '../../store/actions/authActions';
-import { Button, Form, FormGroup, Label, Input, FormText, Container } from 'reactstrap';
+import { connect } from "react-redux";
+import { signUpCom } from "../../store/actions/authActions";
+import firebase from "firebase/app";
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormText,
+  Container,
+} from "reactstrap";
+
+const dbURL = "https://fanding-ab2a1.firebaseio.com/";
 
 class SignUpCom extends Component {
-    state = {
-      email: '',
-      password: '',
-      companyName : '',
-      companyRegistrationNumber: '',
-      corporateRegistrationNumber: '',
-    };
-  
-    handleChange = e => {
-      this.setState({
-        [e.target.id]: e.target.value,
-      });
-    };
-    handleSubmit = e => {
-      e.preventDefault();
-      this.props.signUpCom(this.state);
-    };
-    handleClick = e => {
-      e.preventDefault();
-      //action 추가
-    }
+  state = {
+    email: "",
+    password: "",
+    companyName: "",
+    companyRegistrationNumber: "",
+    corporateRegistrationNumber: "",
+  };
 
-    render() {
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value,
+    });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.signUpCom(this.state);
+  };
 
-      // const { auth, authError } = this.props;
-      // if (auth.uid) return <Redirect to='/' /> 
-      return (
-       
-        <Container className="signform mt-auto">
+  render() {
+    return (
+      <Container className="signform mt-auto">
         <h2>업체 회원가입</h2>
         <Form onSubmit={this.handleSubmit}>
           <FormGroup>
             <Label for="companyName">상호명</Label>
-            <Input type="text" name="companyName" id="companyName"
-            placeholder="상호명을 입력하세요"
-            onChange={this.handleChange}/>
+            <Input
+              type="text"
+              name="companyName"
+              id="companyName"
+              placeholder="상호명을 입력하세요"
+              onChange={this.handleChange}
+            />
           </FormGroup>
           <FormGroup>
             <Label for="companyRegistrationNumber">사업자등록번호</Label>
-            <Button color="warning" className="ml-3" onChange={this.handleClick}>중복확인</Button>
-            <Input type="text" name="companyRegistrationNumber" 
-            id="companyRegistrationNumber"
-            placeholder="사업자등록번호를 입력하세요"
-            onChange={this.handleChange}/>
+            <Button color="warning" className="ml-3" onClick={this.handleClick}>
+              중복확인
+            </Button>
+            <Input
+              type="text"
+              name="companyRegistrationNumber"
+              id="companyRegistrationNumber"
+              placeholder="사업자등록번호를 입력하세요"
+              onChange={this.handleChange}
+            />
           </FormGroup>
           <FormGroup>
             <Label for="corporateRegistrationNumber">법인등록번호</Label>
-            <Input type="text" name="corporateRegistrationNumber" 
-            id="corporateRegistrationNumber"
-            placeholder="법인등록번호를 입력하세요"
-            onChange={this.handleChange}/>
+            <Input
+              type="text"
+              name="corporateRegistrationNumber"
+              id="corporateRegistrationNumber"
+              placeholder="법인등록번호를 입력하세요"
+              onChange={this.handleChange}
+            />
           </FormGroup>
           <FormGroup>
             <Label for="Email">이메일</Label>
-            <Button color="warning" className="ml-3" onChange={this.handleClick}>이메일 인증</Button>
-            <Input type="email" name="email" id="email"
-            placeholder="이메일을 입력하세요" 
-            onChange={this.handleChange}/>
+            <Button
+              color="warning"
+              className="ml-3"
+              onChange={this.handleClick}
+            >
+              이메일 인증
+            </Button>
+            <Input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="이메일을 입력하세요"
+              onChange={this.handleChange}
+            />
           </FormGroup>
           <FormGroup>
             <Label for="Password">비밀번호</Label>
-            <Input type="password" name="password" id="password"
-            placeholder="비밀번호를 입력하세요" 
-            onChange={this.handleChange}/>
+            <Input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="비밀번호를 입력하세요"
+              onChange={this.handleChange}
+            />
           </FormGroup>
-          
-          
+
           <Button>Submit</Button>
         </Form>
       </Container>
-        
-      );
-    }
+    );
   }
+}
 
-  const mapStateToProps = (state) => {
-    return {
-      auth: state.firebase.auth,
-      authError: state.auth.authError
-    }
-  }
+const mapStateToProps = (state) => {
+  return {
+    auth: state.firebase.auth,
+    authError: state.auth.authError,
+  };
+};
 
-  const mapDispatchToProps = (dispatch)=> {
-    return {
-      signUpCom: (creds) => dispatch(signUpCom(creds))
-    }
-  }
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signUpCom: (creds) => dispatch(signUpCom(creds)),
+  };
+};
 
-  export default connect(mapStateToProps, mapDispatchToProps)(SignUpCom)
+export default connect(mapStateToProps, mapDispatchToProps)(SignUpCom);
--- fanding/src/store/actions/authActions.js
+++ fanding/src/store/actions/authActions.js
@@ -1,73 +1,113 @@
-import firebase from 'firebase/app';
+import firebase from "firebase/app";
 
-export const signIn = credentials => {
+export const signIn = (credentials) => {
   return (dispatch, getState) => {
     firebase
       .auth()
       .signInWithEmailAndPassword(credentials.email, credentials.password)
       .then(() => {
-        dispatch({ type: 'LOGIN_SUCCESS' });
+        dispatch({ type: "LOGIN_SUCCESS" });
       })
-      .catch(err => {
-        dispatch({ type: 'LOGIN_ERROR', err });
+      .catch((err) => {
+        dispatch({ type: "LOGIN_ERROR", err });
       });
   };
 };
 
 export const signOut = () => {
   return (dispatch, getState) => {
-    firebase.auth()
-    .signOut()
-    .then(() => {
-      dispatch({ type: 'SIGNOUT_SUCCESS' })
-    });
-  }
-}
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        dispatch({ type: "SIGNOUT_SUCCESS" });
+      });
+  };
+};
 
-export const signUp = newUser => {
+export const signUp = (newUser) => {
   const firestore = firebase.firestore();
   return (dispatch, getState) => {
     firebase
       .auth()
       .createUserWithEmailAndPassword(newUser.email, newUser.password)
-      .then(resp => {
+      .then((userCredential) => {
+        const currentUser = {
+          emailVerified: userCredential.user.emailVerified,
+        };
         return firestore
-          .collection('users')
+          .collection("users")
           .add({
-            artist : newUser.artist
+            artist: newUser.artist,
+            emailVerified: currentUser.emailVerified,
           })
           .then(() => {
-            dispatch({ type: 'SIGNUP_SUCCESS' });
+            dispatch({ type: "SIGNUP_SUCCESS" });
           })
-          .catch(err => {
-            dispatch({ type: 'SIGNUP_ERROR', err });
+          .catch((err) => {
+            dispatch({ type: "SIGNUP_ERROR", err });
+          })
+          .then(() => {
+            let user = firebase.auth().currentUser;
+            user
+              .sendEmailVerification()
+              .then(function () {
+                dispatch({ type: "EMAIL_SPENT_SUCCESS" });
+              })
+              .catch((err) => {
+                dispatch({ type: "NOT_EMAIL_SPENT", err });
+              });
           });
       });
   };
-
 };
 
-export const signUpCom = newCompany => {
+export const signUpCom = (newCompany) => {
   const firestore = firebase.firestore();
-  return (dispatch, getState)=>{
+  return (dispatch, getState) => {
     firebase
-    .auth()
-    .createUserWithEmailAndPassword(newCompany.email, newCompany.password)
-    .then(resp => {
-      return firestore
-      .collection('companys')
-      .add({
-        companyName : newCompany.companyName,
-        companyRegistrationNumber : newCompany.companyRegistrationNumber,
-        corporateRegistrationNumber : newCompany.corporateRegistrationNumber,
-      })
-      .then(() => {
-        dispatch({type: 'SIGNUP_SUCCESS' });
-      })
-      .catch(err => {
-        dispatch({ type: 'SIGNUP_ERROR', err });
+      .auth()
+      .createUserWithEmailAndPassword(newCompany.email, newCompany.password)
+      .then((userCredential) => {
+        const currentUser = {
+          emailVerified: userCredential.user.emailVerified,
+        };
+        return firestore
+          .collection("companys")
+          .add({
+            companyName: newCompany.companyName,
+            companyRegistrationNumber: newCompany.companyRegistrationNumber,
+            corporateRegistrationNumber: newCompany.corporateRegistrationNumber,
+            emailVerified: currentUser.emailVerified,
+          })
+          .then(() => {
+            dispatch({ type: "SIGNUP_SUCCESS" });
+          })
+          .catch((err) => {
+            dispatch({ type: "SIGNUP_ERROR", err });
+          })
+          .then(() => {
+            let company = firebase.auth().currentUser;
+            company
+              .sendEmailVerification()
+              .then(function () {
+                dispatch({ type: "EMAIL_SPENT_SUCCESS" });
+              })
+              .catch((err) => {
+                dispatch({ type: "NOT_EMAIL_SPENT", err });
+              });
+          });
       });
-    });
-}
-}
+  };
+};
 
+// export const comRegCheck = (newCompany) => {
+//   const firestore = firebase.firestore();
+//   return (dispatch, getState) => {
+//     firebase.auth().then(() => {
+//       return firestore
+//         .collection("companys")
+
+//     });
+//   };
+// };
