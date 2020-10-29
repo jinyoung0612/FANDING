c4bbe3c98aac117f196961961e7f2b92eba8db14
JaeWook Jeong <br12345678@ajou.ac.kr>
2020-10-29 13:51:01 +0900

    이메일 인증 및 사업자 등록번호 중복인증

--- fanding/src/components/auth/SignUpCom.js
+++ fanding/src/components/auth/SignUpCom.js
@@ -1,96 +1,139 @@
-import React,{Component} from 'react';
-import { Redirect } from 'react-router-dom'
-import { connect } from 'react-redux';
-import { signUpCom } from '../../store/actions/authActions';
-import { Button, Form, FormGroup, Label, Input, FormText, Container } from 'reactstrap';
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { signUpCom } from "../../store/actions/authActions";
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormText,
+  Container,
+} from "reactstrap";
+import firebase from "firebase/app";
 
 class SignUpCom extends Component {
-    state = {
-      email: '',
-      password: '',
-      companyName : '',
-      companyRegistrationNumber: '',
-      corporateRegistrationNumber: '',
-    };
-  
-    handleChange = e => {
-      this.setState({
-        [e.target.id]: e.target.value,
+  state = {
+    email: "",
+    password: "",
+    companyName: "",
+    companyRegistrationNumber: "",
+    corporateRegistrationNumber: "",
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value,
+    });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.signUpCom(this.state);
+  };
+  handleClick = () => {
+    firebase
+      .firestore()
+      .collection("companies")
+      .where("company_reg_num", "==", this.state.companyRegistrationNumber)
+      .get()
+      .then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+          console.log(doc.id, "=>", doc.data());
+          console.log("Duplicate Appears");
+        });
+      })
+      .catch(function (error) {
+        console.log("Error getting documents: ", error);
       });
-    };
-    handleSubmit = e => {
-      e.preventDefault();
-      this.props.signUpCom(this.state);
-    };
-    handleClick = e => {
-      e.preventDefault();
-      //action 추가
-    }
+  };
 
-    render() {
-
-      const { auth, authError } = this.props;
-      if (auth.uid) return <Redirect to='/' />
-      return (
-       
-        <Container className="signform mt-auto">
+  render() {
+    const { auth, authError } = this.props;
+    if (auth.uid) return <Redirect to="/" />;
+    return (
+      <Container className="signform mt-auto">
         <h2>업체 회원가입</h2>
         <Form onSubmit={this.handleSubmit}>
           <FormGroup>
             <Label for="companyName">상호명</Label>
-            <Input type="text" name="companyName" id="companyName"
-            placeholder="상호명을 입력하세요"
-            onChange={this.handleChange}/>
+            <Input
+              type="text"
+              name="companyName"
+              id="companyName"
+              placeholder="상호명을 입력하세요"
+              onChange={this.handleChange}
+            />
           </FormGroup>
           <FormGroup>
             <Label for="companyRegistrationNumber">사업자등록번호</Label>
-            <Button color="warning" className="ml-3" onChange={this.handleClick}>중복확인</Button>
-            <Input type="text" name="companyRegistrationNumber" 
-            id="companyRegistrationNumber"
-            placeholder="사업자등록번호를 입력하세요"
-            onChange={this.handleChange}/>
+            <Button color="warning" className="ml-3" onClick={this.handleClick}>
+              중복확인
+            </Button>
+            <Input
+              type="text"
+              name="companyRegistrationNumber"
+              id="companyRegistrationNumber"
+              placeholder="사업자등록번호를 입력하세요"
+              onChange={this.handleChange}
+            />
           </FormGroup>
           <FormGroup>
             <Label for="corporateRegistrationNumber">법인등록번호</Label>
-            <Input type="text" name="corporateRegistrationNumber" 
-            id="corporateRegistrationNumber"
-            placeholder="법인등록번호를 입력하세요"
-            onChange={this.handleChange}/>
+            <Input
+              type="text"
+              name="corporateRegistrationNumber"
+              id="corporateRegistrationNumber"
+              placeholder="법인등록번호를 입력하세요"
+              onChange={this.handleChange}
+            />
           </FormGroup>
           <FormGroup>
             <Label for="Email">이메일</Label>
-            <Button color="warning" className="ml-3" onChange={this.handleClick}>이메일 인증</Button>
-            <Input type="email" name="email" id="email"
-            placeholder="이메일을 입력하세요" 
-            onChange={this.handleChange}/>
+            <Button
+              color="warning"
+              className="ml-3"
+              onChange={this.handleClick}
+            >
+              이메일 인증
+            </Button>
+            <Input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="이메일을 입력하세요"
+              onChange={this.handleChange}
+            />
           </FormGroup>
           <FormGroup>
             <Label for="Password">비밀번호</Label>
-            <Input type="password" name="password" id="password"
-            placeholder="비밀번호를 입력하세요" 
-            onChange={this.handleChange}/>
+            <Input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="비밀번호를 입력하세요"
+              onChange={this.handleChange}
+            />
           </FormGroup>
-          
-          
+
           <Button>Submit</Button>
         </Form>
       </Container>
-        
-      );
-    }
+    );
   }
+}
 
-  const mapStateToProps = (state) => {
-    return {
-      auth: state.firebase.auth,
-      authError: state.auth.authError
-    }
-  }
+const mapStateToProps = (state) => {
+  return {
+    auth: state.firebase.auth,
+    authError: state.auth.authError,
+  };
+};
 
-  const mapDispatchToProps = (dispatch)=> {
-    return {
-      signUpCom: (creds) => dispatch(signUpCom(creds))
-    }
-  }
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signUpCom: (creds) => dispatch(signUpCom(creds)),
+  };
+};
 
-  export default connect(mapStateToProps, mapDispatchToProps)(SignUpCom)
+export default connect(mapStateToProps, mapDispatchToProps)(SignUpCom);
--- fanding/src/store/reducers/authReducer.js
+++ fanding/src/store/reducers/authReducer.js
@@ -1,45 +1,55 @@
-
 // import firebase from 'firebase/app';
 const initState = {
-    authError: null
-  }
+  authError: null,
+};
 const authReducer = (state = initState, action) => {
-    switch(action.type){
-      case 'LOGIN_ERROR':
-        console.log('login error');
-        return {
-          ...state,
-          authError: 'Login failed'
-        }
-  
-      case 'LOGIN_SUCCESS':
-        console.log('login success');
-        return {
-          ...state,
-          authError: null
-        }
-  
-      case 'SIGNOUT_SUCCESS':
-        console.log('signout success');
-        return state;
-  
-      case 'SIGNUP_SUCCESS':
-        console.log('signup success')
-        return {
-          ...state,
-          authError: null
-        }
-  
-      case 'SIGNUP_ERROR':
-        console.log('signup error')
-        return {
-          ...state,
-          authError: action.err.message
-        }
-  
-      default:
-        return state
-    }
-  };
-  
-  export default authReducer;
+  switch (action.type) {
+    case "LOGIN_ERROR":
+      console.log("login error");
+      return {
+        ...state,
+        authError: "Login failed",
+      };
+
+    case "LOGIN_SUCCESS":
+      console.log("login success");
+      return {
+        ...state,
+        authError: null,
+      };
+
+    case "SIGNOUT_SUCCESS":
+      console.log("signout success");
+      return state;
+
+    case "SIGNUP_SUCCESS":
+      console.log("signup success");
+      return {
+        ...state,
+        authError: null,
+      };
+
+    case "SIGNUP_ERROR":
+      console.log("signup error");
+      return {
+        ...state,
+        authError: action.err.message,
+      };
+    case "NOT_EMAIL_SPENT":
+      console.log("not email spent");
+      return {
+        ...state,
+        authError: action.err.message,
+      };
+    case "EMAIL_SPENT_SUCCESS":
+      console.log("email spent success");
+      return {
+        ...state,
+        authError: null,
+      };
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
