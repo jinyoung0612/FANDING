ff8c4b3d2ff5aaa02c6903e9382e48db4a4be635
jell <jl1096@ajou.ac.kr>
2020-11-07 17:49:59 +0900

    change chongdae_auth page

--- fanding/src/components/layout/Chongdae_auth.js
+++ fanding/src/components/layout/Chongdae_auth.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Button } from 'reactstrap';
+import { Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
 import { Link } from "react-router-dom";
 
 class Chongdae_auth extends Component{
@@ -7,19 +7,26 @@ class Chongdae_auth extends Component{
     render()
     {
         return(
-            
-            <React.Fragment>
-            <Link to="/identity_auth">
-              <Button>본인 인증</Button>  
-            </Link>
-            <Link to="/account_auth">
-              <Button>계좌 인증</Button>  
-            </Link>
-            </React.Fragment>
-        
+          <Card body>
+                <CardTitle>본인 인증</CardTitle>
+                <CardText>펀딩을 생성하려면 본인 인증이 필요합니다.</CardText>
+                <Link to="/account_auth">
+                  <Button color="warning" onClick={chongdae_auth} >본인 인증</Button>
+                </Link>
+          </Card>  
         );
     }
 }
+
+function chongdae_auth(){
+  const chongdaeVerify = window.open(
+    "https://testapi.openbanking.or.kr/oauth/2.0/authorize?"
+    +"response_type=code&client_id=qhsl7X3L59LPtU6QfdZNv2d4jYYKKFiY8K2iw2NI&"
+    +"redirect_uri=http://localhost:3000/account_auth&"
+    +"scope=login inquiry transfer&state=12345678901234567890123456789012&auth_type=0&lang=kor"
+  )
+};
+
 
 
 export default Chongdae_auth;
