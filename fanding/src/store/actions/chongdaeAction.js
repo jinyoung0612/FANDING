8dc877542ac9ab5fbfc77990aa48b27b869c5dc3
jell <jl1096@ajou.ac.kr>
2020-11-09 19:03:15 +0900

    add chongdae collection in firestore and verify reducer

---
+++ fanding/src/store/actions/chongdaeAction.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app';
+import 'firebase/storage';
+import {createAction} from 'redux-actions';
+
+const CHONGDAE_SAVE = 'CHONGDAE_SAVE';
+
+export const chongdae_save = createAction(CHONGDAE_SAVE);
+
+export const verifyChongdae = (newChongdae) => {
+    return (dispatch, getFiresotre) => {
+        const firestore = getFiresotre();
+        const user = firebase.auth().currentUser;
+
+        firestore.collection("chongdaes").add({
+            user_uid: user.uid,
+            user_email: user.email,
+            access_token: newChongdae.access_token,
+            refresh_token: newChongdae.refresh_token,
+            user_seq_no: newChongdae.user_seq_no,
+        }).then(()=>{
+            dispatch({type: "VERIFYCHONGDAE_SUCCESS",newChongdae});
+        }).catch((err) => {
+            dispatch({type: "VERIFYCHONGDAE_ERROR", err});
+        })
+    };
+};
