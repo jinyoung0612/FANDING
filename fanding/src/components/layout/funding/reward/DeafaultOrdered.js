d0be71a3525fffcf70a3bab1cb045abb53609f13
Ji Eun Han <hanji0514@ajou.ac.kr>
2020-11-14 18:01:37 +0900

    completed funding filtering in the newest order

---
+++ fanding/src/components/layout/funding/reward/DeafaultOrdered.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+//import Dropdown from './Dropdown';
+import { compose } from 'redux';
+import FundingList from './RewardFundingList';
+
+class DefaultOrdered extends Component {
+    constructor({someProp}){
+        super()
+        this.state = {someProp}
+    }
+    // componentWillReceiveProps({someProp}) {
+    //     this.setState({...this.state,someProp})
+    // }
+    
+    render()
+    {   console.log("default Ordered");
+        const { fundings } = this.props;
+        
+        return(
+            <div onClick={this.props.onClick}>
+            <FundingList fundings={fundings} />
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+    console.log(state);
+    return {
+        fundings: state.firestore.ordered.fundings
+    }
+}
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([//data sync
+        { collection: 'fundings',
+          where: ['fundingType', '==', 'reward'],
+        } 
+    ])
+)(DefaultOrdered);
--- fanding/src/components/layout/funding/reward/RewardFunding.js
+++ fanding/src/components/layout/funding/reward/RewardFunding.js
@@ -1,29 +1,107 @@
 import React, { Component } from "react";
+import { DefaultContext } from "react-icons";
 import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
+import {ButtonDropdown, DropdownItem, DropdownMenu, DropdownToggle} from "reactstrap";
+//import Dropdown from './Dropdown';
 import { compose } from 'redux';
 import FundingList from './RewardFundingList';
+import NewOrdered from './NewOrdered';
+import DefaultOrdered from './DeafaultOrdered';
+import PopOrdered from './PopOrdered';
 
 class RewardFunding extends Component {
-    render(){
-        const { fundings } = this.props;
 
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.changeValue = this.changeValue.bind(this);
+        this.state = {
+            actions: [],
+            dropDownValue: '보기 방식',
+            dropdownOpen: false
+        };
+    }
+    toggle(event) {
+
+        this.setState({
+            dropdownOpen: !this.state.dropdownOpen
+        });
+    }
+
+    changeValue(e) {
+        this.setState({dropDownValue: e.currentTarget.textContent},
+            ()=>{this.forceUpdate()});
+        //let id = e.currentTarget.getAttribute("id");
+        //console.log(this.state.dropDownValue);
+    }
+
+    // shouldComponentUpdate(nextProps, nextState) {
+    //     console.log("next State: ", nextState.dropDownValue);
+    //     console.log("current State: ", this.state.dropDownValue);
+    //     return this.state.dropDownValue !== nextState.dropDownValue;
+    //   }
+
+    render(){
+        //const { fundings } = this.props;
+        
         return(
-            <FundingList fundings={fundings} />
+            <div>
+
+                <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+
+                    <DropdownToggle caret>
+                        {this.state.dropDownValue}
+                    </DropdownToggle>
+                    <DropdownMenu>
+                            {/* {this.state.actions.map(e => {
+                                return <DropdownItem id={e.id} key={e.id} onClick={this.changeValue}>{e.name}</DropdownItem>
+                            })} */}
+                        <DropdownItem>
+                            <div onClick={this.changeValue}>보기 방식</div>
+                        </DropdownItem>
+                        <DropdownItem>
+                            <div onClick={this.changeValue}>최신순</div>
+                        </DropdownItem>
+                        {/* <DropdownItem>
+                            <div onClick={this.changeValue}>인기순</div>  */}
+                                {/* 참여퍼센테이지가 높은 순*/}
+                        {/* </DropdownItem> */}
+                            
+
+                    </DropdownMenu>
+
+            </ButtonDropdown>
+            {/* {this.state.dropDownValue==="보기 방식" ? <DefaultOrdered />
+            : this.state.dropDownValue==="최신순"? <NewOrdered />
+            : <PopOrdered />} */}
+            
+            {this.state.dropDownValue === "보기 방식" && <DefaultOrdered onClick={this.changeValue}/>}
+            {this.state.dropDownValue === "최신순" && <NewOrdered onClick={this.changeValue}/>}
+            {this.state.dropDownValue === "인기순" && <PopOrdered onClick={this.changeValue}/>}
+            
+
+            </div>
+            
         )
     }
 }
-const mapStateToProps = (state) => {
-    console.log(state);
-    return {
-        fundings: state.firestore.ordered.fundings
-    }
-}
-export default compose(
-    connect(mapStateToProps),
-    firestoreConnect([//data sync
-        { collection: 'fundings',
-        where: ['fundingType', '==', 'reward'],
-        } 
-    ])
-)(RewardFunding);
+
+// const mapStateToProps = (state) => {
+//     console.log(state);
+//     return {
+//         fundings: state.firestore.ordered.fundings
+//     }
+// }
+// export default compose(
+//     connect(mapStateToProps),
+//     firestoreConnect([//data sync
+//         { collection: 'fundings',
+//           where: ['fundingType', '==', 'reward'],
+          
+//         } 
+//     ])
+// )(RewardFunding);
+
+export default RewardFunding;
