b85b77107f3c9ff810de9bb0d2632846389091e2
jell <jl1096@ajou.ac.kr>
2020-11-21 02:06:30 +0900

    change chongdaeAction Code

--- src/store/actions/chongdaeAction.js
+++ src/store/actions/chongdaeAction.js
@@ -1,6 +1,8 @@
 import firebase from 'firebase/app';
 import 'firebase/storage';
+import { connect } from 'react-redux';
 import {createAction} from 'redux-actions';
+import {loadFundings} from './userActions';
 
 const CHONGDAE_SAVE = 'CHONGDAE_SAVE';
 
@@ -11,12 +13,18 @@ export const verifyChongdae = (newChongdae) => {
         const firestore = firebase.firestore();
         const user = firebase.auth().currentUser;
 
-        firestore.collection("chongdaes").add({
+        // console.log(newChongdae)
+        firestore
+        .collection("chongdaes")
+        .doc()
+        .set({
             user_uid: user.uid,
             user_email: user.email,
             access_token: newChongdae.access_token,
             refresh_token: newChongdae.refresh_token,
             user_seq_no: newChongdae.user_seq_no,
+            account_num: newChongdae.account_num,
+            bank_name: newChongdae.bank_name,
         }).then(()=>{
             dispatch({type: "VERIFYCHONGDAE_SUCCESS",newChongdae});
         }).catch((err) => {
@@ -25,11 +33,28 @@ export const verifyChongdae = (newChongdae) => {
     };
 };
 
-/*
-export const transactionList = (newTransaction) => {
-    return (dispatch) => {
+
+export const getTransactionList = (newTransactionList) => {
+    
+    return (dispatch,getFirestore) => {
         const firestore = firebase.firestore();
         const user = firebase.auth().currentUser;
+        console.log(newTransactionList);
 
-    }
-}*/
+        firestore
+        .collection("transactionLists")
+        .doc()
+        .set({
+            chongdae_uid: user.uid,
+            chongdae_email: user.email,
+            fintech_use_num: newTransactionList.fintech_use_num,
+            user_name: newTransactionList.user_name,
+            bank_name: newTransactionList.bank_name,
+        }).then(()=>{
+            dispatch({type: "GET_TRANSACTION_LIST_SUCCESS",newTransactionList});
+        }).catch((err) => {
+            dispatch({type: "GET_TRANSACTION_LIST_ERROR", err});
+        });
+    };
+};
+
