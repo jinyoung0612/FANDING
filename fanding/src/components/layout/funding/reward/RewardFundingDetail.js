2621ede751a096d4a72711a805e0cde2473dae95
Ji Eun Han <hanji0514@ajou.ac.kr>
2020-11-21 17:12:34 +0900

    edited RewardFundingDetail

--- fanding/src/components/layout/funding/reward/RewardFundingDetail.js
+++ fanding/src/components/layout/funding/reward/RewardFundingDetail.js
@@ -23,32 +23,7 @@ let imgStyle = {
     maxWidth: '700px'
   }
   
- //다음 우편번호 API
-//  const Postcode = () => {
-//   const handleComplete = (data) => {
-//     let fullAddress = data.address;
-//     let extraAddress = ''; 
-    
-//     if (data.addressType === 'R') {
-//       if (data.bname !== '') {
-//         extraAddress += data.bname;
-//       }
-//       if (data.buildingName !== '') {
-//         extraAddress += (extraAddress !== '' ? `, ${data.buildingName}` : data.buildingName);
-//       }
-//       fullAddress += (extraAddress !== '' ? ` (${extraAddress})` : '');
-//     }
 
-//     console.log(fullAddress);  // e.g. '서울 성동구 왕십리로2길 20 (성수동1가)'
-//   }
-
-//   return (
-//     <DaumPostcode
-//       onComplete={handleComplete}
-//       { ...props }
-//     />
-//   );
-// }
 class FundingDetail extends Component{
 
   constructor(props){
