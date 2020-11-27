4702857c85791ff46a2bf4a17c0d96e71bfdb139
jell <jl1096@ajou.ac.kr>
2020-11-27 14:19:27 +0900

    Code modification according to server change

--- fanding/src/components/layout/Chongdae_auth.js
+++ fanding/src/components/layout/Chongdae_auth.js
@@ -122,7 +122,9 @@ class Chongdae_auth extends Component{
                 </Col>
                 <Col sm="4">
                     <Button block style={{marginTop:'90px'}} id="verButton" color="warning" 
-                    href="https://testapi.openbanking.or.kr/oauth/2.0/authorize?response_type=code&client_id=qhsl7X3L59LPtU6QfdZNv2d4jYYKKFiY8K2iw2NI&redirect_uri=http://localhost:3000/account_auth&scope=login inquiry transfer&state=12345678901234567890123456789012&auth_type=0&lang=kor" >본인 인증</Button>
+                    href="https://testapi.openbanking.or.kr/oauth/2.0/authorize?response_type=code&client_id=qhsl7X3L59LPtU6QfdZNv2d4jYYKKFiY8K2iw2NI&redirect_uri=http://localhost:3000/account_auth&scope=login inquiry transfer&state=12345678901234567890123456789012&auth_type=0&lang=kor" 
+                    //href="https://testapi.openbanking.or.kr/oauth/2.0/authorize?response_type=code&client_id=qhsl7X3L59LPtU6QfdZNv2d4jYYKKFiY8K2iw2NI&redirect_uri=http://118.67.131.132:3000/account_auth&scope=login inquiry transfer&state=12345678901234567890123456789012&auth_type=0&lang=kor" 
+                    >본인 인증</Button>
                 </Col>
 
               </Row>  
