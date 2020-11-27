f70097a4b2e65ec1915bbd128b60afe2d6d30fce
jell <jl1096@ajou.ac.kr>
2020-11-28 01:07:53 +0900

    add notice item code

---
+++ fanding/src/components/notice/NoticeItem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { notice_read, firebase_notice_remove } from '../../store/actions/noticeAction'
+
+const NoticeItem = ({row, index, notice_read, firebase_notice_remove}) => (
+    <tr>
+        <td>{index}</td>
+        <td><a onClick={() => { notice_read(row.ntcno) } }>{row.ntctitle}</a></td>
+        <td>{row.ntcwriter}</td>
+        <td>{row.ntcdate}</td>
+        <td><a onClick={() => { firebase_notice_remove(row.ntcno) }}>X</a></td>
+    </tr>
+);
+
+const mapDispatchToProps = dispatch => ({
+  notice_read: ntcno => dispatch(notice_read(ntcno)),
+  firebase_notice_remove: ntcno => dispatch(firebase_notice_remove(ntcno))
+})
+
+export default connect(null, mapDispatchToProps)(NoticeItem)
