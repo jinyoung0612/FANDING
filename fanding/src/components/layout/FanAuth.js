5b3c4f7c9fd229bfdff5dbb81bae85c5d1180bc2
Ji Eun Han <hanji0514@ajou.ac.kr>
2020-12-02 23:46:25 +0900

    edited datails in FanAuth page

--- fanding/src/components/layout/FanAuth.js
+++ fanding/src/components/layout/FanAuth.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
-import {Container, Form, FormGroup, Label, CustomInput, Media} from 'reactstrap';
+import {Container, Form, FormGroup, Label, CustomInput, Media, Button} from 'reactstrap';
 import ticketImg from './ticket.png';
 import streamingImg from './streaming.png';
+import fanclub from './fanclub.png';
 const imgStyle = {
     maxHeight: 400,
     maxWidth: 200
@@ -20,10 +21,11 @@ class FanAuth extends Component{
            <Container
            style={{backgroundColor:"#fafafa", borderRadius:"10px", padding:"3em 2em"}}>
             <p>팬 인증은 <strong>스트리밍 인증, 팬클럽 인증, 공연 관람 내역 중 최소 하나</strong>를 하셔야합니다.</p>
+            <p>팬 인증은 펀딩 참여자가 펀딩을 고르는 하나의 기준이 될 수 있습니다.</p>
+            <p>모든 사진에는 도용 방지를 위해 <strong>자신의 닉네임, 날짜, 시간</strong>이 나올 수 있게 해주세요.</p>
             <Form>
                 <FormGroup>
                 <Label><strong>1. 스트리밍 인증하기</strong></Label>
-                {/* {예시이미지, 이미지 넣는 창} */}
                 <Media object src={streamingImg} style={imgStyle}/>
                 <CustomInput type="file" id="streamingImage" name="customFile"
                                      label="스트리밍 인증 캡처를 업로드 하세요"
@@ -33,7 +35,7 @@ class FanAuth extends Component{
             
                 <FormGroup>
                 <Label><strong>2. 팬클럽 인증하기</strong></Label>
-                {/* {예시이미지, 이미지 넣는 창} */}
+                <Media object src={fanclub} style={imgStyle}/>
                 <CustomInput type="file" id="fanclubImage" name="customFile"
                                      label="팬클럽 인증 캡처를 업로드 하세요"
                                      onChange={this.handleImageChange}
@@ -42,7 +44,6 @@ class FanAuth extends Component{
             
                 <FormGroup>
                 <Label><strong>3. 공연 관람 내역 인증하기</strong></Label>
-                {/* {예시이미지, 이미지 넣는 창} */}
                 <Media object src={ticketImg} style={imgStyle}/>
                 <CustomInput type="file" id="concertImage" name="customFile"
                                      label="공연 관람 내역을 업로드 하세요"
@@ -52,12 +53,13 @@ class FanAuth extends Component{
             
                 <FormGroup>
                 <Label><strong>4. 앨범 및 공식 굿즈 인증하기</strong></Label>
-                {/* {예시이미지, 이미지 넣는 창} */}
                 <CustomInput type="file" id="albumImage" name="customFile"
                                      label="앨범 및 공식 굿즈를 업로드 하세요"
                                      onChange={this.handleImageChange}
                         />
                 </FormGroup>
+                <Button color="warning" size="md" block >제출하기</Button>
+                {/* 버튼 handler 연결해야함 */}
             </Form>
 
          </Container>
