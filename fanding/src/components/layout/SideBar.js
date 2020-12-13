ee4a496c51d4c48332781bf63acdbe109415be81
Ji Eun Han <hanji0514@ajou.ac.kr>
2020-12-13 13:10:25 +0900

    onging sidebar

--- fanding/src/components/layout/SideBar.js
+++ fanding/src/components/layout/SideBar.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, Component} from "react";
 import { NavItem, NavLink, Nav, Collapse } from "reactstrap";
 import { FaTools } from "react-icons/fa";
 import { BsPersonCheckFill, BsFileText, BsFillHeartFill, BsBuilding } from "react-icons/bs";
@@ -85,6 +85,50 @@ const SideBar = ({ isOpen, toggle }) => (
     </div>
   </div>
 );
+const TopNavBar = ({ isOpen, toggle }) => (
+  // <div className={classNames("sidebar", { "is-open": isOpen })}>
+  //   <div className="sidebar-header">
+  //     <span color="info" onClick={toggle} style={{ color: "#fafafa" }}>
+  //       &times;
+  //     </span>
+  //     <h3 style={{color: 'black'}}>내 정보</h3>
+  //   </div>
+  //   <div className="side-menu">
+      <Nav vertical className="list-unstyled pb-3">
+        <NavItem>
+          <NavLink size={12} className="inactive" activeClassName="active" tag={Link} to={"/myaccount"}>
+            <FaTools className="mr-2" />
+            {/* <FontAwesomeIcon icon={faBriefcase} className="mr-2" /> */}
+            내 정보 관리
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink className="inactive" activeClassName="active" tag={Link} to={"/chongdae"}>
+            <BsPersonCheckFill className="mr-2" />
+            총대 인증
+          </NavLink>
+        </NavItem>
+        <NavItem>
+        <SubMenu title="펀딩 관리" icon={BsFileText} items={submenus[0]} />
+          <NavLink className="inactive" activeClassName="active" tag={Link} to={"/faq"}>
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink className="inactive" activeClassName="active" tag={Link} to={"/myRecruit"}>
+            <BsBuilding className="mr-2" />
+            업체 모집글 관리
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink className="inactive" activeClassName="active" tag={Link} to={"/myaccount"}>
+            <BsFillHeartFill className="mr-2" />
+            마이 위시리스트
+          </NavLink>
+        </NavItem>
+      </Nav>
+  //   </div>
+  // </div>
+);
 
 const submenus = [
   [
@@ -99,5 +143,7 @@ const submenus = [
   ],
 ];
 
-export default SideBar;
+
+
+ export default SideBar;
 
